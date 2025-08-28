fbd335f946df3f11ea8e9686a50a371f1164ea08
melwin3210 <melwin3210@gmail.com>
2025-08-28 11:23:38 +0530

    initial commit

---
+++ big-calendar/src/data/dummyData.js
@@ -0,0 +1,16 @@
+const dummyData = {
+  "01-09-2025": [
+    { user: "user_1", value: 1 },
+    { user: "user_2", value: 2 },
+    { user: "user_3", value: 3 },
+    { user: "user_4", value: 4 }
+  ],
+  "02-09-2025": [
+    { user: "user_1", value: 2 },
+    { user: "user_2", value: 4 },
+    { user: "user_3", value: 6 },
+    { user: "user_4", value: 8 }
+  ]
+};
+
+export default dummyData;
