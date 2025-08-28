9460cfdee549cdd26c2986cf440416a146b57d8f
melwin3210 <melwin3210@gmail.com>
2025-08-28 11:35:01 +0530

    redux store provide in root level

--- big-calendar/src/main.jsx
+++ big-calendar/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { Provider } from "react-redux";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from "./redux/store/store.js";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <Provider store={store}>
+      <App />
+   </Provider>
 )
